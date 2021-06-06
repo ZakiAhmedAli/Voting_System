e3170c9625cb7bcb0c4ca55b8a5e038f662295ab
zaki <zakiahmedhamed@gmail.com>
2021-06-06 18:37:36 +0200

    add function update to main

--- APP/main.c
+++ APP/main.c
@@ -45,66 +45,7 @@ int main(void) {
 
 while(1)
 {
-	vote = USART_u8RXChar();
-
-	if(vote == 1+48)
-	{
-		counter1++;
-		LCD_GoToXy(1,2);
-		LCD_VoidWriteNum(counter1);
-	}
-	else if(vote == 2+48)
-	{
-		counter2++;
-		LCD_GoToXy(1,14);
-		LCD_VoidWriteNum(counter2);
-	}
-	else if(vote == 3+48)
-	{
-		counter3++;
-		LCD_GoToXy(2,2);
-		LCD_VoidWriteNum(counter3);
-	}
-	else if(vote == 4+48)
-	{
-		counter4++;
-		LCD_GoToXy(2,14);
-		LCD_VoidWriteNum(counter4);
-	}
-	else if(vote == '0' )
-	{
-		if(counter1 > counter2 && counter1 > counter3 &&counter1 > counter4)
-		{
-			LCD_voidClear();
-			LCD_VoidWriteString((u8*)"A is the Win");
-		}
-		if(counter2 > counter1 && counter2 > counter3 &&counter2 > counter4)
-		{
-			LCD_voidClear();
-			LCD_VoidWriteString((u8*)"B is the Win");
-		}
-		if(counter3 > counter1 && counter3 > counter2 &&counter3 > counter4)
-		{
-			LCD_voidClear();
-			LCD_VoidWriteString((u8*)"C is the Win");
-		}
-		if(counter4 > counter2 && counter4 > counter3 &&counter4 > counter1)
-		{
-			LCD_voidClear();
-			LCD_VoidWriteString((u8*)"D is the Win");
-		}
-
-		_delay_ms(2000);
-		LCD_voidClear();
-		counter1 = counter2 =counter3=counter4=0;
-		LCD_VoidWriteString((u8*)"A=0");
-		LCD_GoToXy(1,12);
-		LCD_VoidWriteString((u8*)"B=0");
-		LCD_GoToXy(2,0);
-
-		LCD_VoidWriteString((u8*)"C=0");
-		LCD_GoToXy(2,12);
-		LCD_VoidWriteString((u8*)"D=0");
+	UART_voidUpdta();
 
 	}
 }
